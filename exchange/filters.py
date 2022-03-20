53e376ea82cd2daaf2b9b09b6cc63d3f9439d95d
Alt+F4 <mail@example.com>
2022-03-20 13:01:53 +0100

    Fixed Labels

--- exchange/filters.py
+++ exchange/filters.py
@@ -6,4 +6,9 @@ class ExchangeListingFilter(django_filters.FilterSet):
     class Meta:
         model = ExchangeListing
         fields = ['exchange_from', 'exchange_to']
+    def __init__(self, *args, **kwargs):
+       super(ExchangeListingFilter, self).__init__(*args, **kwargs)
+       self.filters['exchange_from'].label="Wymień na"
+       self.filters['exchange_to'].label="Wymień z"
+
 
